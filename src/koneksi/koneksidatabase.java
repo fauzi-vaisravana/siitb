7dc6d4f7fd93f3a85caf7cefe05ec9ef230a4a52
Fauzi <uzimuhammad418@gmail.com>
2025-03-19 18:50:57 +0700

    first commit

---
+++ src/koneksi/koneksidatabase.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Fauzi
+ */
+public class koneksidatabase {
+     public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/siitb";
+            String user = "root"; // Sesuaikan dengan user XAMPP
+            String password = ""; // Kosong jika default XAMPP
+            Connection conn = DriverManager.getConnection(url, user, password);
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Koneksi Gagal: " + e.getMessage());
+            return null;
+        }
+    }
+}
